16714b15d6a1b95cb22b29092edf00c46806165b
dhruv1707 <dmehrottra007@gmail.com>
2025-05-18 15:51:50 -0400

    Fixed min_frequency to train the tokenizer

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ def get_or_build_tokenizer(config, ds, language):
     if not Path.exists(tokenizer_path):
         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
         tokenizer.pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"])
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
         tokenizer.train_from_iterator(get_all_sentences(ds, language), trainer)
         tokenizer.save(str(tokenizer_path))
     else:
